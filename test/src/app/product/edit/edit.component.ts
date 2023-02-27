7b95368c4addb4bf18736067ade421a5556e4220
conPuppy <hoangnhatnam29102000@gmail.com>
2023-02-27 08:37:22 +0700

    up2

--- test/src/app/product/edit/edit.component.ts
+++ test/src/app/product/edit/edit.component.ts
@@ -12,37 +12,37 @@ import {ProductServiceService} from 'src/app/service/product-service.service';
 export class EditComponent implements OnInit {
   id: any;
   product: Product | undefined
-  formEdit!: FormGroup;
+  formEdit: FormGroup  = new FormGroup({
+    id: new FormControl(0),
+    name: new FormControl("0"),
+    img: new FormControl("0"),
+    price: new FormControl("0")
+  });
 
 
-  constructor(private router: Router, private productService: ProductServiceService, private route: ActivatedRoute) {
-
-  }
-
-  ngOnInit(): void {
-    this.id = this.route.snapshot.paramMap.get('id');
-    if (this.id != null) {
-      console.log(this.id);
-      this.formEdit = new FormGroup({
-        id: new FormControl(0),
-        name: new FormControl("0"),
-        img: new FormControl("0"),
-        price: new FormControl("0")
-      });
-      this.showEdit(this.id);
-
-    }
-  }
-
-  showEdit(id: number) {
-    this.productService.findProductById(this.id).subscribe((data) => {
-      this.product = data;
-      this.formEdit.get("id")?.setValue(this.id);
-      this.formEdit.get("name")?.setValue(this.product.name);
-      this.formEdit.get("img")?.setValue(this.product.img);
-      this.formEdit.get("price")?.setValue(this.product.price);
-    })
-  }
+  // constructor(private router: Router, private productService: ProductServiceService, private route: ActivatedRoute) {
+  //
+  // }
+  //
+  // ngOnInit(): void {
+  //   this.id = this.route.snapshot.paramMap.get('id');
+  //   if (this.id != null) {
+  //     console.log(this.id);
+  //    
+  //     this.showEdit(this.id);
+  //
+  //   }
+  // }
+  //
+  // showEdit(id: number) {
+  //   this.productService.findProductById(this.id).subscribe((data) => {
+  //     this.product = data;
+  //     this.formEdit.get("id")?.setValue(this.id);
+  //     this.formEdit.get("name")?.setValue(this.product.name);
+  //     this.formEdit.get("img")?.setValue(this.product.img);
+  //     this.formEdit.get("price")?.setValue(this.product.price);
+  //   })
+  // }
   edit() {
       this.productService.updateProduct(this.id,this.formEdit.value).subscribe((data)=>{
         alert("vao");
@@ -56,27 +56,27 @@ export class EditComponent implements OnInit {
   // formEdit!: FormGroup;
   //
   //
-  // constructor(private router: Router, private productService: ProductServiceService, private route: ActivatedRoute) {
-  //
-  // }
-  //
-  // ngOnInit(): void {
-  //   this.id = this.route.snapshot.paramMap.get('id');
-  //   if (this.id != null) {
-  //     console.log(this.id);
-  //     this.productService.findProductById(this.id).subscribe(data=>{
-  //       this.product = data;
-  //       console.log(this.product);
-  //       console.log(this.product.name);
-  //       this.formEdit = new FormGroup({
-  //         name: new FormControl(this.product?.name),
-  //         img: new FormControl(this.product?.img),
-  //         price: new FormControl(this.product?.price)
-  //       })
-  //     })
-  //
-  //   }
-  // }
+  constructor(private router: Router, private productService: ProductServiceService, private route: ActivatedRoute) {
+
+  }
+
+  ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id');
+    if (this.id != null) {
+      console.log(this.id);
+      this.productService.findProductById(this.id).subscribe(data=>{
+        this.product = data;
+        console.log(this.product);
+        console.log(this.product.name);
+        this.formEdit = new FormGroup({
+          name: new FormControl(this.product?.name),
+          img: new FormControl(this.product?.img),
+          price: new FormControl(this.product?.price)
+        })
+      })
+
+    }
+  }
 
 
 }
